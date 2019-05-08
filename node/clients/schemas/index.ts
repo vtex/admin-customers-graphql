f1f1b225ebcfe1a247655d703b808daf23266fed
Ana Luiza <analuizamtg@gmail.com>
2019-05-08 16:57:13 -0300

    Use IOClient instead of IODataSource :rocket:

--- node/clients/schemas/index.ts
+++ node/clients/schemas/index.ts
@@ -1,17 +1,14 @@
-import { IODataSource } from '@vtex/api'
+import { JanusClient } from '@vtex/api'
 
 import { DATA_ENTITY, SCHEMA_NAME } from '../../utils/constants'
-import factory from '../client'
 import { withAuthToken } from '../../resolvers/headers'
 
-class SchemaDataSource extends IODataSource {
-  protected httpClientFactory = factory
-
+class Schemas extends JanusClient {
   public get = (vtexIdToken: string) =>
-    this.http.get(`${DATA_ENTITY}/schemas/${SCHEMA_NAME}`, {
+    this.http.get(`api/dataentities/${DATA_ENTITY}/schemas/${SCHEMA_NAME}`, {
       metric: 'crm-get-schema',
       headers: withAuthToken()(vtexIdToken),
     })
 }
 
-export default SchemaDataSource
+export default Schemas
