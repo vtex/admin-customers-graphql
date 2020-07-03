3bf7d631411e6f3cb0ab7e62e9a1421b4e344fe0
Marcos Candeia <marrcooos@gmail.com>
2020-07-03 10:30:17 -0300

    Fixing schema get

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- Use list schema API instead of getting it directly
+
 ## [0.11.0] - 2019-07-03
 
 ### Changed
--- node/resolvers/schemas/index.ts
+++ node/resolvers/schemas/index.ts
@@ -1,17 +1,28 @@
-import { prop, difference, reduce } from 'ramda'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AuthenticationError } from '@vtex/api'
+import { difference, find, prop, propEq, reduce } from 'ramda'
+import Maybe from 'graphql/tsutils/Maybe'
+
+import { DataEntitySchema } from '../../clients/schemas'
 import { defaultFields } from '../../utils'
+import { SCHEMA_NAME } from '../../utils/constants'
 
 export const queries = {
-  schema: (_: any, __: any, ctx: Context) => {
+  schema: async (_: unknown, __: unknown, ctx: Context) => {
     const {
       clients: { schemas },
       cookies,
     } = ctx
+
     const vtexIdToken = cookies.get('VtexIdclientAutCookie')
+
     if (!vtexIdToken) throw new AuthenticationError('User is not logged in')
 
-    return schemas.get(vtexIdToken)
+    const dataEntitySchema: Maybe<DataEntitySchema> = await schemas
+      .list(vtexIdToken)
+      .then(find<DataEntitySchema>(propEq('name', SCHEMA_NAME)))
+
+    return dataEntitySchema?.schema
   },
 }
 
@@ -22,6 +33,7 @@ export const fieldResolvers = {
     vIndexed: prop('v-indexed'),
     customFields: ({ properties }: any) => {
       const customFields = difference(Object.keys(properties), defaultFields)
+
       return reduce(
         (acc, field) => acc.concat({ key: field, value: properties[field] }),
         [] as any,
